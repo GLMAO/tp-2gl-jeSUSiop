1fcf110eb21a3a9abfe83eb44441849fa8c1c00a
rahmanou <abderrahmane.bel12@gmail.com>
2025-11-20 13:36:29 +0100

    Observer ready

--- src/main/java/com/polytech/tp/GestionnaireEmploiDuTemps.java
+++ src/main/java/com/polytech/tp/GestionnaireEmploiDuTemps.java
@@ -5,21 +5,39 @@ import java.util.List;
 
 public class GestionnaireEmploiDuTemps {
     private List<ICours> listeCours = new ArrayList<>();
+    private List<Observer> observers = new ArrayList<>();
 
     public void ajouterCours(ICours cours) {
         this.listeCours.add(cours);
         System.out.println("Nouveau cours ajouté : " + cours.getDescription());
         // TODO: C'est ici qu'il faudrait notifier les étudiants (Observer pattern)
+        notifyObservers("New cours : " + cours.getDescription());
     }
 
     public void modifierCours(ICours cours, String message) {
         // Logique de modification...
         System.out.println("Cours modifié : " + message);
         // TODO: Notifier les observateurs ici aussi
+        notifyObservers("Cours modified : " + message);
     }
 
     public void setChangement(String string) {
+        notifyObservers(string);
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'setChangement'");
     }
+    public void addObserver(Observer o) {
+        observers.add(o);
+    }
+
+    public void removeObserver(Observer o) {
+        observers.remove(o);
+    }
+
+    private void notifyObservers(String message) {
+        for (Observer o : observers) {
+            o.update(message);
+        }
+    }
+
 }
